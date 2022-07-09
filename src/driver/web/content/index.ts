c788dcde836356f0d16a0866b8f993d3ef4fb361
ylc395 <cyl@cyl.moe>
2022-07-09 20:26:08 +0800

    add settimeout for highlight

--- src/driver/web/content/index.ts
+++ src/driver/web/content/index.ts
@@ -3,4 +3,13 @@ import { createTooltip } from './capture';
 import highlight from './highlight';
 
 document.addEventListener('selectionchange', debounce(createTooltip, 1000));
+
+const timer = setTimeout(() => {
+  window.removeEventListener('load', highlight);
+  highlight();
+}, 3000);
+
 window.addEventListener('load', highlight);
+window.addEventListener('load', () => {
+  clearTimeout(timer);
+});
